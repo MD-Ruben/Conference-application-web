acb6dff4ca24361dd141bc0834c64b4403fdd60b
yab21 <yablaiyablairubenvirgil@gmail.com>
2024-10-12 17:20:32 -1200

    initial commit

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,179 @@
+/* eslint-disable camelcase */
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import HomeCard from "./HomeCard";
+import MeetingModal from "./MeetingModal";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useUser } from "@clerk/nextjs";
+import Loader from "./Loader";
+import { Textarea } from "./ui/textarea";
+import ReactDatePicker from "react-datepicker";
+import { useToast } from "@/hooks/use-toast";
+import { Input } from "./ui/input";
+
+const initialValues = {
+  dateTime: new Date(),
+  description: "",
+  link: "",
+};
+
+const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
+  const [values, setValues] = useState(initialValues);
+  const [callDetail, setCallDetail] = useState<Call>();
+  const client = useStreamVideoClient();
+  const { user } = useUser();
+  const { toast } = useToast();
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      if (!values.dateTime) {
+        toast({ title: "Veuillez sélectionner une date et une heure" });
+        return;
+      }
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+      if (!call) throw new Error("Échec de la création de la réunion");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Réunion instantanée";
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetail(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: "Réunion créée",
+      });
+    } catch (error) {
+      console.error(error);
+      toast({ title: "Échec de la création de la réunion" });
+    }
+  };
+
+  if (!client || !user) return <Loader />;
+
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetail?.id}`;
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="Nouvelle réunion"
+        description="Démarrer une réunion instantanée"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Rejoindre la réunion"
+        description="via un lien d'invitation"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Calendrier des réunions"
+        description="Planifiez votre réunion"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="Voir les enregistrements"
+        description="Enregistrements des réunions"
+        className="bg-yellow-1"
+        handleClick={() => router.push("/recordings")}
+      />
+
+      {!callDetail ? (
+        <MeetingModal
+          isOpen={meetingState === "isScheduleMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title="Créer une réunion"
+          handleClick={createMeeting}
+        >
+          <div className="flex flex-col gap-2.5">
+            <label className="text-base font-normal leading-[22.4px] text-sky-2">
+              Ajouter une description
+            </label>
+            <Textarea
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex w-full flex-col gap-2.5">
+            <label className="text-base font-normal leading-[22.4px] text-sky-2">
+              Sélectionner la date et l'heure
+            </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
+          </div>
+        </MeetingModal>
+      ) : (
+        <MeetingModal
+          isOpen={meetingState === "isScheduleMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title="Réunion créée"
+          className="text-center"
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: "Lien copié" });
+          }}
+          image={"/icons/checked.svg"}
+          buttonIcon="/icons/copy.svg"
+          buttonText="Copier le lien de la réunion"
+        />
+      )}
+
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Tapez le lien ici"
+        className="text-center"
+        buttonText="Participer à la conférence"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Lien de la réunion"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModal>
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Démarrer une réunion instantanée"
+        className="text-center"
+        buttonText="Débuter de la réunion"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+    {
+      imgURL: '/icons/Home.svg',
+      route: '/',
+      label: 'Accueil',
+    },
+  
+    {
+      imgURL: '/icons/upcoming.svg',
+      route: '/upcoming',
+      label: 'À venir',
+    },
+    {
+      imgURL: '/icons/previous.svg',
+      route: '/previous',
+      label: 'Précédent',
+    },
+    {
+      imgURL: '/icons/Video.svg',
+      route: '/recordings',
+      label: 'Enregistrements',
+    },
+    {
+      imgURL: '/icons/add-personal.svg',
+      route: '/personal-room',
+      label: 'Espace personnel',
+    },
+  ];
+  
+  export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+  ];
